7f8f087b0784bce2156190c364924293984d5c50
Mateusz <56366696+mateuszkierznowski@users.noreply.github.com>
2023-05-03 10:13:06 +0200

    colab path update

--- exps/default/yolox_s.py
+++ exps/default/yolox_s.py
@@ -13,3 +13,8 @@ class Exp(MyExp):
         self.depth = 0.33
         self.width = 0.50
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+        #self.data_dir = "/content/YOLOX/datasets/COCO/Yolox_dataset"
+        #self.train_ann = "train/labels.json"
+        #self.val_ann = "validation/labels.json"
+        self.max_epoch = 3
+        self.num_classes = 4
