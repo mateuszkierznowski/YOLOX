baa9eb27b2b65b5a35c875c38acd251c317e4d15
Mateusz <56366696+mateuszkierznowski@users.noreply.github.com>
2023-05-03 10:11:27 +0200

    colab update

--- yolox/data/datasets/coco.py
+++ yolox/data/datasets/coco.py
@@ -39,8 +39,8 @@ class COCODataset(CacheDataset):
     def __init__(
         self,
         data_dir=None,
-        json_file="instances_train2017.json",
-        name="train2017",
+        json_file="annotations.json",
+        name="ppes-custom-dataset/train",
         img_size=(416, 416),
         preproc=None,
         cache=False,
@@ -60,7 +60,7 @@ class COCODataset(CacheDataset):
         self.data_dir = data_dir
         self.json_file = json_file
 
-        self.coco = COCO(os.path.join(self.data_dir, "annotations", self.json_file))
+        self.coco = COCO(os.path.join(self.data_dir, "ppes-custom-dataset/train", "_annotations.coco.json"))
         remove_useless_info(self.coco)
         self.ids = self.coco.getImgIds()
         self.num_imgs = len(self.ids)
